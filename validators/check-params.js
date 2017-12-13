cb891d1a45763ad9ad40934b8de42bf7270692d0
Kenneth Larsen <kennethlarsen@protonmail.com>
2017-12-13 14:32:34 +0100

    :shirt: Lint fix

--- validators/check-params.js
+++ validators/check-params.js
@@ -1,19 +1,19 @@
 const colors = require('colors/safe');
 
 function paramsLenght() {
-  this.checkParams = function(line, file) {
+  this.checkParams = function (line, file) {
     if (line.includes('function')) {
       const regExp = /\(([^)]+)\)/;
       const params = regExp.exec(line);
       const paramsArray = params[1].split(',');
-  
+
       if (paramsArray.length > 4) {
         process.stdout.write(colors.red('Error! More than 4 args in a function. Consider refactoring\n'));
-        process.stdout.write(file + '\n');
-        process.stdout.write(line + '\n');
+        process.stdout.write(`${file}\n`);
+        process.stdout.write(`${line}\n`);
       }
     }
-  }
+  };
 }
 
 module.exports = paramsLenght;
