a7421c5abcad6b1977212c2b08e76dce21cb6c63
Kenneth Larsen <kennethlarsen@protonmail.com>
2017-12-14 12:23:58 +0100

    :shirt: lint fixes

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const readline = require('readline');
 const stream = require('stream');
 const ParamsLenght = require('./lib/validators/function-validator');
 const LineCheck = require('./lib/validators/file-length');
-const Report = require('./lib/file-handling/write-report');
 
 const paramsCheckInstance = new ParamsLenght();
 const lineCheckInstance = new LineCheck();
-const report = new Report();
 
 const walkPath = 'test/';
 
 function walk(dir, done) {
-  fs.readdir(dir, function (error, list) {
+  fs.readdir(dir, (error, list) => {
+    let i = 0;
+
     if (error) {
       return done(error);
     }
-
-    var i = 0;
 
     (function next () {
       var file = list[i++];
@@ -30,26 +28,26 @@ function walk(dir, done) {
       file = dir + '/' + file;
       
       fs.stat(file, function (error, stat) {
-  
-          if (stat && stat.isDirectory()) {
+
+        if (stat && stat.isDirectory()) {
               walk(file, function (error) {
                   next();
               });
-          } else {
-              const instream = fs.createReadStream(file);
-              const outstream = new stream;
-              const rl = readline.createInterface(instream, outstream);
+        } else {
+          const instream = fs.createReadStream(file);
+          const outstream = new stream;
+          const rl = readline.createInterface(instream, outstream);
 
-              rl.on('line', function(line) {
-                paramsCheckInstance.checkParams(line, file);
-              });
+          rl.on('line', function(line) {
+            paramsCheckInstance.checkParams(line, file);
+          });
 
-              rl.on('close', function() {
-                lineCheckInstance.checkLenghtOfFile(file);
-                next();
-              });
-          }
-        });
+          rl.on('close', function() {
+            lineCheckInstance.checkLenghtOfFile(file);
+            next();
+          });
+        }
+      });
     })();
   });
 };
@@ -59,11 +57,11 @@ process.stdout.write('processing...\n');
 process.stdout.write('-------------------------------------------------------------\n');
 
 walk(walkPath, function(error) {
-    if (error) {
-        throw error;
-    } else {
-        process.stdout.write('-------------------------------------------------------------\n');
-        process.stdout.write('finished.\n');
-        process.stdout.write('-------------------------------------------------------------\n');
-    }
+  if (error) {
+    throw error;
+  } else {
+    process.stdout.write('-------------------------------------------------------------\n');
+    process.stdout.write('finished.\n');
+    process.stdout.write('-------------------------------------------------------------\n');
+  }
 });
