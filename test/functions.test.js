ef9968a78246406a98fb5c254d16f6f6a5c81b99
Mads Obel <madsobelcph@gmail.com>
2018-04-10 19:46:01 +0200

    Added some tests for functions

---
+++ test/functions.test.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const mock = require('mock-fs');
+const mocha = require('mocha');
+const chai = require('chai');
+
+const ParamsLenght = require('../lib/validators/function-validator');
+
+const paramsCheckInstance = new ParamsLenght();
+
+mocha.describe('Functions', () => {
+  mocha.beforeEach(() => {
+    mock({
+      'report.md': '',
+    });
+  });
+
+  mocha.it('should not write to report if it finds a function with one param', () => {
+    paramsCheckInstance.checkParams('function someFunc(foo) {', 'somefile.js', './report.md');
+    const reportAfterCheck = fs.readFileSync('./report.md').toString();
+    chai.expect(reportAfterCheck).to.be.lengthOf(0);
+    chai.expect(reportAfterCheck).not.to.contain('(too-many-args)');
+  });
+
+  mocha.it('should write to report if it finds a function with five params', () => {
+    paramsCheckInstance.checkParams('function someFunc(foo, bar, baz, qux, quux) {', 'somefile.js', './report.md');
+    const reportAfterCheck = fs.readFileSync('./report.md').toString();
+    chai.expect(reportAfterCheck).not.to.be.lengthOf(0);
+    chai.expect(reportAfterCheck).to.contain('(too-many-args)');
+  });
+
+  mocha.after(() => {
+    mock.restore();
+  });
+});
